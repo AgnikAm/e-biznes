062c71c574ee4fdae264d44fc563b7f7d43ded51
AgnikAm <KingaAleksandraKlima@gmail.com>
2025-04-28 21:00:06 +0200

    Add base for bot and ability to write and receive messages from it

---
+++ zad3/src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+import net.dv8tion.jda.api.requests.GatewayIntent
+
+fun main() {
+    startKtorServer()
+
+    val token = "INSERT_TOKEN"
+
+    val jda = JDABuilder.createDefault(token)
+        .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+        .setActivity(Activity.playing("with Ktor"))
+        .addEventListeners(CommandListener())
+        .build()
+
+    jda.updateCommands().addCommands(
+        Commands.slash("hello", "Say hello to the bot")
+    ).queue()
+
+    jda.awaitReady()
+    println("Bot is ready and commands are registered!")
+}
+
+class CommandListener : ListenerAdapter() {
+    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        when (event.name) {
+            "hello" -> {
+                event.reply("Hi! I'm KtorBot").queue()
+            }
+        }
+    }
+}
+
+fun startKtorServer() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Discord Bot is running!")
+            }
+            get("/status") {
+                call.respondText("Bot is online!")
+            }
+        }
+    }.start(wait = false)
+}
